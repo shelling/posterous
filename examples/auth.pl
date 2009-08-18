5315cde7eecab618b395a4f620e103ec6f22f476
shelling <navyblueshellingford@gmail.com>
2009-08-19 01:17:19 +0800

    add examples

---
+++ examples/auth.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+
+use 5.010;
+use lib qw(lib);
+
+use Posterous;
+use Data::Dumper;
+use YAML qw(LoadFile);
+
+$config = LoadFile("$ENV{HOME}/.posterous");
+
+$posterous = Posterous->new($config->{core}->{user}, $config->{core}->{pass});
+
+say Dumper($posterous);
+
+say $posterous->account_info;
